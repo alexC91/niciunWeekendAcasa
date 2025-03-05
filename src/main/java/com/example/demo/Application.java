0e910b498013cb56ed1a97183a0e294b54e591cd
daniel <danielpopescu2611@gmail.com>
2025-03-05 15:22:05 +0200

    rugaciuni

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -1,6 +1,6 @@
 package com.example.demo;
 
-import com.com.email.test.EmailSender;
+// import com.com.email.test.EmailSender;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -15,6 +15,6 @@ public class Application {
 		System.out.println("Application started");
 
 		System.out.println("Trimitere email de test...");
-		EmailSender.sendEmail("niciunWeekendAcasa1@gmail.com", "Test Email", "Acesta este un email de test din Java.");
+		// EmailSender.sendEmail("niciunWeekendAcasa1@gmail.com", "Test Email", "Acesta este un email de test din Java.");
 	}
 }
