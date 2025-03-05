4c462e96cb327243bf8363f7c5a545a5c647dbea
mihaivasile17 <Vmihai739@gmail.com>
2025-03-05 11:02:12 +0200

    mail_testare
    testare1
    
    Co-Authored-By: MaraCalugaru <166919051+MaraCalugaru@users.noreply.github.com>

---
+++ src/main/java/com/com.turism.mail/Main.java
@@ -0,0 +1,11 @@
+package com.turism.email;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Testare trimitere email...");
+        EmailSender.sendEmail("destinatar@example.com", "Subiect Test", "Acesta este un e-mail de test.");
+
+        System.out.println("Testare primire email...");
+        EmailReceiver.receiveEmails();
+    }
+}
