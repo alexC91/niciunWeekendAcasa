bc61847b83c74daef6ad215a2abe5203be35f078
daniel <danielpopescu2611@gmail.com>
2025-03-05 14:54:39 +0200

    ceva merge

--- src/main/java/com/example/demo/GreetingController.java
+++ src/main/java/com/example/demo/GreetingController.java
@@ -2,12 +2,13 @@ package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
 
-@RestController
+@Controller
 public class GreetingController {
 
-    @GetMapping("/greet")
-    public String greet() {
-        return "Hey there!";
+    @GetMapping("/")
+    public String home() {
+        return "index";
     }
 }
