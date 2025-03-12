b60dcff4084ee8095beaf7ac52981868105b6f74
MateiDima <mateidima2003@gmail.com>
2025-03-12 15:47:57 +0200

    remodificat users.java

--- src/main/java/com/linkDatabase/Users.java
+++ src/main/java/com/linkDatabase/Users.java
@@ -2,31 +2,55 @@ package com.linkDatabase;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Table(name="app_users")
 public class Users {
-    @Column(nullable=false, unique=true)
+    @Column(name="ID", nullable=false, unique=true)
     private Integer id;
 
-    @Column(nullable=false)
-    private String email;
-
-    @Column(nullable=false)
-    private String firstName;
-
-    @Column(nullable=false)
+    @Column(name="last_name", nullable=false)
     private String lastName;
 
-    @Column(nullable=false)
+    @Column(name="first_name", nullable=false)
+    private String firstName;
+
+    @Column(name="email", nullable=false)
+    private String email;
+
+    @Column(name="pass", nullable=false)
     private String password;
 
-    @Column
+    @Column(name="is_activated")
+    private Byte isActivated;
+
+    @Column(name="is_disabled")
+    private Byte isDisabled;
+
+    @Column(name="created_at")
     private LocalDateTime createdAt;
 
     @Column
     private String role;
+
+    public Byte getIsDisabled() {
+        return isDisabled;
+    }
+
+    public void setIsDisabled(Byte isDisabled) {
+        this.isDisabled = isDisabled;
+    }
+
+    public Byte getIsActivated() {
+        return isActivated;
+    }
+
+    public void setIsActivated(Byte isActivated) {
+        this.isActivated = isActivated;
+    }
 
     public Integer getId() {
         return id;
