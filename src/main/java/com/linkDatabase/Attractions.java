a18ab3b1a336d5c782ab23be070293530ae73fbe
MateiDima <mateidima2003@gmail.com>
2025-03-12 11:14:12 +0200

    Entitati pentru conectare baza de date la java. Kiss kiss

---
+++ src/main/java/com/linkDatabase/Attractions.java
@@ -0,0 +1,71 @@
+package com.linkDatabase;
+
+
+@Entity
+public class Attractions {
+    @Column(nullable=false, unique=true)
+    private Integer id;
+
+    @Column(nullable=false)
+    private String nume;
+
+    @Column
+    private String details;
+
+    @Column
+    private String location;
+
+    @Column
+    private Float overallRating;
+
+    @Column
+    private Integer numberOfRatings;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNume() {
+        return nume;
+    }
+
+    public void setNume(String nume) {
+        this.nume = nume;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Float getOverallRating() {
+        return overallRating;
+    }
+
+    public void setOverallRating(Float overallRating) {
+        this.overallRating = overallRating;
+    }
+
+    public Integer getNumberOfRatings() {
+        return numberOfRatings;
+    }
+
+    public void setNumberOfRatings(Integer numberOfRatings) {
+        this.numberOfRatings = numberOfRatings;
+    }
+}
