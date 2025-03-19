058212c2950438f47fa52e10e973b9fdf1b85a78
AlexC <alex.costache@ingenios.ro>
2025-03-19 15:09:20 +0200

    Revert "Merge branch 'main' into test-html"
    This reverts commit b850a46012220930e621aecaca4f91567d22b26c, reversing
    changes made to 12e61a0031293d5aaf48652f8296de355cac898a.

--- src/main/java/com/example/demo/GreetingController.java
+++ src/main/java/com/example/demo/GreetingController.java
@@ -2,13 +2,30 @@ package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
 
-@RestController
+@Controller
 public class GreetingController {
 
-    @GetMapping("/greet")
-    public String greet() {
-        return "Hey there!";
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+    @GetMapping("/about")
+    public String about() {
+        return "about.html";
+    }
+    @GetMapping("/services")
+    public String services() {
+        return "services.html";
+    }
+    @GetMapping("/blog")
+    public String blog() {
+        return "blog.html";
+    }
+    @GetMapping("/contact")
+    public String contact() {
+        return "contact.html";
     }
     @GetMapping("/login")
     public String login() {
