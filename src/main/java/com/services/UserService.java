766ed6b8c6bfea615166df29385ee9823744717f
AlexC <alex.costache@ingenios.ro>
2025-03-19 11:03:10 +0200

    connect db

---
+++ src/main/java/com/services/UserService.java
@@ -0,0 +1,15 @@
+package com.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public void doMagic() {
+        userRepository.findAll();
+    }
+}
