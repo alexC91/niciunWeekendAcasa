fd4538b922d6aefbd2f082e58d20f04e0028dbcd
AlexC <alex.costache@ingenios.ro>
2025-03-20 15:31:52 +0200

    adaugat interfata

--- src/main/java/com/example/demo/GreetingController.java
+++ src/main/java/com/example/demo/GreetingController.java
@@ -6,14 +6,36 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
-    @GetMapping("/greet")
-    public String greet() {
-        return "Hey there!";
+    @GetMapping("/")
+    public String home() {
+        return "index";
     }
+
+    @GetMapping("/about")
+    public String about() {
+        return "about.html";
+    }
+
+    @GetMapping("/services")
+    public String services() {
+        return "services.html";
+    }
+
+    @GetMapping("/blog")
+    public String blog() {
+        return "blog.html";
+    }
+
+    @GetMapping("/contact")
+    public String contact() {
+        return "contact.html";
+    }
+
     @GetMapping("/login")
     public String login() {
         return "login.html";
     }
+
     @GetMapping("/register")
     public String register() {
         return "signup.html";
