95f5fc29a279359d06cb6d0e7ebb1f233e6f1a65
MateiDima <mateidima2003@gmail.com>
2025-05-07 11:48:16 +0300

    buton counties + putin citiescounties

---
+++ src/main/java/com/repositories/CitiesCountiesRepository.java
@@ -0,0 +1,9 @@
+//package com.repositories;
+//
+//import com.linkDatabase.CitiesCounties;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//
+//@Repository
+//public interface CitiesCountiesRepository extends JpaRepository<CitiesCounties, Long> {
+//}
--- src/main/java/com/repositories/CitiesRepository.java
+++ src/main/java/com/repositories/CitiesRepository.java
@@ -1,7 +1,6 @@
 package com.repositories;
 
 import com.linkDatabase.Cities;
-import com.linkDatabase.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
